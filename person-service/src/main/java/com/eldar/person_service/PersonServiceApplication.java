a8f7538d229c5b0ad088f649d2ff611aa5334c86
MatiBatista99 <matias.batista@eldars.com.ar>
2025-01-22 09:56:30 -0300

    Se agregaron implementaciones varias

--- person-service/src/main/java/com/eldar/person_service/PersonServiceApplication.java
+++ person-service/src/main/java/com/eldar/person_service/PersonServiceApplication.java
@@ -1,17 +1,15 @@
-package com.eldar.admin_server;
+package com.eldar.person_service;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAdminServer
-public class AdminServerApplication {
+public class PersonServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdminServerApplication.class, args);
+		SpringApplication.run(PersonServiceApplication.class, args);
 	}
 
 }
