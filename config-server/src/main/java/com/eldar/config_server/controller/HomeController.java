9dbc8cded16156adf614a98044d13a18cf67f8ee
Bautista Basilio <bautista.basilio@eldars.local>
2025-02-20 13:45:31 -0300

    Integracion de logstash

--- config-server/src/main/java/com/eldar/config_server/controller/HomeController.java
+++ config-server/src/main/java/com/eldar/config_server/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.eldar.config_server.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
     @GetMapping
     public String home() {
+
+        log.info("Prueba de log");
         return "Bienvenido al ConfigServer";
     }
 }
