64d6695dfe19e245513db6e84142285fb532c262
Bautista Basilio <bautista.basilio@eldars.local>
2025-02-21 12:10:09 -0300

    Correccion de errores para el funcionamiento de logstash

--- config-server/src/main/java/com/eldar/config_server/controller/HomeController.java
+++ config-server/src/main/java/com/eldar/config_server/controller/HomeController.java
@@ -1,21 +1,19 @@
 package com.eldar.config_server.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HomeController {
 
 
-    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping
     public String home() {
-
-        log.info("START - getAllCustomers");
-        log.info("END - getAllCustomers");
+        log.info("START - handleException");
+        log.info("END - handleException");
         return "Bienvenido al ConfigServer";
 
     }
