8dff64e243ef2710536d4cc59f9c53e3f00c508b
MatiBatista99 <matias.batista@eldars.com.ar>
2025-02-25 14:37:07 -0300

    no se

--- config-server/src/main/java/com/eldar/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/eldar/config_server/ConfigServerApplication.java
@@ -2,24 +2,32 @@ package com.eldar.config_server;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+
+public class ConfigServerApplication implements CommandLineRunner {
 
 	@Value("${spring.cloud.config.server.git.username}")
-	private static String gitUsername;
-
+	private String getUsername;
 	@Value("${spring.cloud.config.server.git.password}")
-	private static String gitPassword;
+	private String getPassword;
+
 
 	public static void main(String[] args) {
-		System.out.println("Git Username:"+ gitUsername);
-		System.out.println("Git Password:"+ gitUsername);
+
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("GitUsername: " + getUsername);
+		System.out.println("GitPassword: " + getPassword);
+	}
 }
