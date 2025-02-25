601d2709887c564378bd80ab443b4d487ff81efb
MatiBatista99 <matias.batista@eldars.com.ar>
2025-02-25 14:32:42 -0300

    Cambis

--- config-server/src/main/java/com/eldar/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/eldar/config_server/ConfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.eldar.config_server;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -8,7 +10,15 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigServerApplication {
 
+	@Value("${spring.cloud.config.server.git.username}")
+	private static String gitUsername;
+
+	@Value("${spring.cloud.config.server.git.password}")
+	private static String gitPassword;
+
 	public static void main(String[] args) {
+		System.out.println("Git Username:"+ gitUsername);
+		System.out.println("Git Password:"+ gitUsername);
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
