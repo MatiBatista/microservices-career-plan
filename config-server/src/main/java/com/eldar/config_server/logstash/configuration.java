3de74f75e24d4c2c25c96d863db7ecaa877b4b71
Bautista Basilio <76666780+Bautistadev@users.noreply.github.com>
2025-03-11 12:32:40 -0300

    Update configuration.java

--- config-server/src/main/java/com/eldar/config_server/logstash/configuration.java
+++ config-server/src/main/java/com/eldar/config_server/logstash/configuration.java
@@ -45,6 +45,7 @@ public class configuration {
 
             LogstashEncoder logstashEncoder = new LogstashEncoder();
             logstashEncoder.setContext(context);
+            logstashEncoder.addCustomField("serviceName", serviceName);
             logstashEncoder.start();
             logstashAppender.setEncoder(logstashEncoder);
             logstashAppender.start();
